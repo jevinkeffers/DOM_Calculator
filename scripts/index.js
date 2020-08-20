954ab1782523d9f793b53602630f88932d4d62b5
Kevin Jeffers <jevinkeffers@gmail.com>
2020-08-19 20:49:35 -0400

    built the HTML and CSS; JS work in progress

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// PEMDAS
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const ops = ['*', '/', '+', '-']
+
+// const display = document.getElementById("input");
+// const numKeys = document.querySelectorAll(".number");
+// const opKeys = document.querySelectorAll(".operator");
+
+// const displayArr = [];
+
+// numKeys.forEach(function(element) {
+//     element.addEventListener('click', e => {
+//     const target = event.target;
+//     displayArr.push(target.innerHTML)
+//     console.log(displayArr);
+//     }
+// )})
+
+// displayArr.forEach(function(item) {
+//     display.value += item;
+// })
+/////
+
+
+const numbers = [1, 2];
+const operators = ['+'];
+let addition = operators.indexOf("+");
+while (addition !== -1) {
+    console.log("before operation", numbers)
+    numbers.splice(0, number[0] + numbers[1]);
+    console.log("after math operation", numbers);
+    addition = -1;
+    // [3]
+}
+
+//substring
+
+//
